2c91bdc3ccdf1e7ace5fc8bf3d84d9445586e14c
Matvey Zvantsov <zvancov.mu@ispras.ru>
2025-12-25 16:01:20 +0300

    my tests

--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 import pytest
 
-# from model import ...
+from model import Batch, OrderLine, allocate
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
@@ -9,24 +9,48 @@ later = tomorrow + timedelta(days=10)
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=today)
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=today)
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=2)
+
+    assert batch.can_allocate(line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    batch = Batch("batch-001", "SMALL-TABLE", qty=2, eta=today)
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=3)
+
+    assert batch.can_allocate(line)
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=today)
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=20)
+
+    assert batch.can_allocate(line)
 
 
 def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+    warehouse_batch = Batch("warehouse-batch-001", "SMALL-TABLE", qty=2, eta=today)
+    shipment_batch = Batch("shipment-batch-01", "SMALL-TABLE", qty=4, eta=tomorrow)
+
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=1)
+
+    allocate(line, [warehouse_batch, shipment_batch])
 
 
 def test_prefers_earlier_batches():
-    pytest.fail("todo")
+    shipment_batch_1 = Batch("shipment-batch-01", "SMALL-TABLE", qty=2, eta=tomorrow)
+    shipment_batch_2 = Batch("shipment-batch-02", "SMALL-TABLE", qty=4, eta=later)
+
+    line = OrderLine("order-ref", "SMALL-TABLE", qty=1)
+
+    assert allocate(line, [shipment_batch_1, shipment_batch_2]) == "shipment-batch-01"
